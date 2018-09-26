61fc5a0b4ea2eb1d8404735eb4d098f29b7795e1
Timmo404 <cala.stephane@gmail.com>
2018-09-27 01:22:05 +0200

    fichier jS

---
+++ script/main.js
@@ -0,0 +1,8 @@
+/* var monTitre = document.querySelector('h1');
+monTitre.textContent = 'Hello world!'; */
+/* document.querySelector('html').onclick = function() {
+    alert('Aïe, arrêtez de cliquer !!');} */
+
+    setTimeout(function(){
+      alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stplease !!!!!');
+    }, 10000);
